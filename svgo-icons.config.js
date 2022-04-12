52ec2992ed2731a3ea8c885633e30d04f48f8969
Saade <saade@outlook.com.br>
2022-04-12 19:13:00 -0300

    initial commit

---
+++ svgo-icons.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    plugins: [
+        'removeDimensions',
+        'sortAttrs',
+        {
+            name: "removeAttrs",
+            params: {
+                attrs: "(id)"
+            }
+        },
+        {
+            name: 'addAttributesToSVGElement',
+            params: {
+                attributes: [
+                    { fill: 'currentColor' },
+                    { 'aria-hidden': true }
+                ]
+            }
+        }
+    ]
+}
